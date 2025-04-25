cf0ccc1d8fd32126a0b7ca5dd84eec86ae9ddca9
Saradhmn213 <168528137+Saradhmn213@users.noreply.github.com>
2025-04-25 21:16:49 +0200

    Update employe.js

--- backend/models/employe.js
+++ backend/models/employe.js
@@ -1,1 +1,12 @@
+// server/models/Candidat.js
+const mongoose = require('mongoose');
+
+const candidatSchema = new mongoose.Schema({
+  nom: String,
+  email: String,
+  posteRecherche: String,
+  experience: Number
+});
+
+module.exports = mongoose.model('Candidat', candidatSchema);
 
